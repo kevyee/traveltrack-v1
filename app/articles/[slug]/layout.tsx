ef96c30e44ad5516a75235f909be8b25ee7c015c
Kevin Yee <kkevinyee13@gmail.com>
2024-04-01 20:06:45 +1100

    Fixed open graph image

--- app/articles/[slug]/layout.tsx
+++ app/articles/[slug]/layout.tsx
@@ -18,9 +18,11 @@ export async function generateMetadata({
       wordwrap: false,
       limits: { maxInputLength: 160 },
     }) + "...";
-  const image = "https://www.traveltrack.com/img";
-  // post._embedded?.["wp:featuredmedia"]?.[0]?.media_details?.sizes?.large
-  //   .source_url || `${fjord.site_domain}/opengraph-image.jpg`;
+
+  const image = `${fjord.site_domain}/img?url=${
+    post._embedded?.["wp:featuredmedia"]?.[0]?.media_details?.sizes?.large
+      .source_url || `${fjord.site_domain}/opengraph-image.jpg`
+  }`;
 
   const url = `${fjord.site_domain}/articles/${post.slug}`;
 
--- app/img/route.ts
+++ app/img/route.ts
@@ -1,6 +1,8 @@
 export async function GET(request: Request, response: Response) {
+  const { searchParams } = new URL(request.url);
+  const url = searchParams.get("url");
   const imageResponse = await fetch(
-    "https://traveltrack-27e7f5.ingress-baronn.ewp.live/wp-content/uploads/2024/03/d0c86189-b849-4884-9dec-3ac47becf03d-1024x585.jpg"
+    url || "https://www.traveltrack.com/opengraph-image.jpg"
   );
   const imageBuffer = await imageResponse.arrayBuffer();
 
